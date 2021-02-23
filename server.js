8ce9cc8599c06b41855985e5ef743dbde92aad1a
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2021-02-23 12:33:32 +0530

    name change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/event_planner'));
+app.use(express.static(__dirname + '/dist/event-planner-website'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/event_planner/index.html'));
+res.sendFile(path.join(__dirname+'/dist/event-planner-website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
